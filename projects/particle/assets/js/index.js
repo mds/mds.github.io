d87e0cd330fd42c1a2ab0b89d93a8c9979e945a4
Matt D. Smith <matt@mds.is>
2016-09-19 13:50:28 -0400

    updated emit svg icons and ton of particle updates

--- projects/particle/assets/js/index.js
+++ projects/particle/assets/js/index.js
@@ -6,47 +6,30 @@ $( document ).ready(function() {
   new Floatl(document.querySelectorAll('.js-floatl')[2])
   new Floatl(document.querySelectorAll('.js-floatl')[3])
 
-  // // ----------------------------------------------
-  // // random colors
-  // var randomColorArray = ['red','blue','green'],
-  // useThisColor = randomColorArray[Math.floor(Math.random() * randomColorArray.length)];
+  // validate email
+  $('#btnValidate').click(function() {
+    var sEmail = $('#txtEmail').val();
+    // Checking Empty Fields
+    if ($.trim(sEmail).length == 0 || $("#fname").val()=="" || $("#lname").val()=="") {
+      alert('All fields are mandatory');
+      e.preventDefault();
+    }
+    if (validateEmail(sEmail)) {
+      alert('Nice!! your Email is valid, now you can continue..');
+    } else {
+      alert('Invalid Email Address');
+      e.preventDefault();
+    }
+  };
 
-  // // find and replace colors
-  // function colorReplace(findHexColor, replaceWith) {
-  //   // Convert rgb color strings to hex
-  //   function rgb2hex(rgb) {
-  //     if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-  //     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  //     function hex(x) {
-  //       return ("0" + parseInt(x).toString(16)).slice(-2);
-  //     }
-  //     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-  //   }
-
-  //   // Select and run a map function on every tag
-  //   $('*').map(function(i, el) {
-  //     // Get the computed styles of each tag
-  //     var styles = window.getComputedStyle(el);
-
-  //     // Go through each computed style and search for "color"
-  //     Object.keys(styles).reduce(function(acc, k) {
-  //       var name = styles[k];
-  //       var value = styles.getPropertyValue(name);
-  //       if (value !== null && name.indexOf("color") >= 0) {
-  //         // Convert the rgb color to hex and compare with the target color
-  //         if (value.indexOf("rgb(") >= 0 && rgb2hex(value) === findHexColor) {
-  //           // Replace the color on this found color attribute
-  //           $(el).css(name, replaceWith);
-  //         }
-  //       }
-  //     });
-  //   });
-  // }
-
-  // // Call like this for each color attribute you want to replace
-  // colorReplace("#be02e8", useThisColor);
-  // // End find and replace colors
-  // // ----------------------------------------------
+  // Function that validates email address through a regular expression.
+  function validateEmail(sEmail) {
+    var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+    if (filter.test(sEmail)) {
+      return true;
+    } else {
+    return false;
+  }
 
 });
 
